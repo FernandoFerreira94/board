49b7dd5a7bb60e556de2868e61acc071344fd1b0
Fernando <fernandoeqp59@gmail.com>
2025-05-16 22:22:35 -0300

    Finalizado page private, verificação de logado para acessar dashboard, começado a montar page dashbord

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,8 +9,10 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import toast from "react-hot-toast";
 
 export default function Header() {
-  const [isMobile, setIsMobile] = useState(false);
+  // session Verifica se tem usuario logado
+  // status verifica o status "authenticated" , "unauthenticated" ou "loading"
   const { data: session, status } = useSession();
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -20,6 +22,16 @@ export default function Header() {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      toast.success("Você esta logado");
+      return;
+    }
+    if (status === "unauthenticated") {
+      toast.error("Você foi deslogado");
+    }
+  }, [status]);
 
   return (
     <header className="w-full h-20  bg-[#0f0f0f]  ">
@@ -31,16 +43,18 @@ export default function Header() {
             </span>
             <span className="text-red-600 font-bold text-3xl">+</span>
           </Link>
-          <Link
-            className="text-black font-bold bg-white border py-1.5 px-8 rounded-2xl transition duration-500 hover:scale-108  cursor-pointer"
-            href="/dashboard"
-          >
-            {isMobile ? (
-              <MdOutlineDashboardCustomize size={25} />
-            ) : (
-              "Meu painel"
-            )}
-          </Link>
+          {session?.user && (
+            <Link
+              className="text-black font-bold bg-white border py-1.5 px-8 rounded-2xl transition duration-500 hover:scale-108  cursor-pointer"
+              href="/dashboard"
+            >
+              {isMobile ? (
+                <MdOutlineDashboardCustomize size={25} />
+              ) : (
+                "Meu painel"
+              )}
+            </Link>
+          )}
         </nav>
         {status === "loading" ? (
           <>
@@ -61,7 +75,6 @@ export default function Header() {
               <button
                 onClick={() => {
                   signOut();
-                  toast.success("Você foi deslogado");
                 }}
                 className="text-white mt-3 rounded-2xl  cursor-pointer"
               >
@@ -90,7 +103,6 @@ export default function Header() {
           <button
             onClick={() => {
               signIn("google");
-              toast.success("Você esta logado");
             }}
             className="text-white border py-1.5 px-8 rounded-2xl transition duration-500 hover:bg-white hover:text-black  cursor-pointer"
           >
